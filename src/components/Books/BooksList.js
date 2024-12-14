ca2cfba5cb2106e3dffdd6efd9a883ca9d45752d
Adonica <studio@adonicapedraza.com>
2024-12-14 15:55:11 -0800

    Update BooksList.js

--- src/components/Books/BooksList.js
+++ src/components/Books/BooksList.js
@@ -1,11 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Book from "./Book";
 import { BooksContext } from "../../contexts/BooksContext";
 
 function BooksList() {
-  const books = useContext(BooksContext);
-  const bookList = books.map((book) => <Book key={book.bookId} book={book} />);
-  return (
+  const booksContext = useContext(BooksContext);
+  const [ books, setBooks] = useState(booksContext);
+
+    return (
     <div className="container mt-5">
       <section className="d-flex gap-2 flex-wrap align-items-stretch justify-content-center">
         {bookList}
