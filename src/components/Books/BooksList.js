1f39ae5e1b74d5576d468531d1f1277e92d132ab
Adonica <studio@adonicapedraza.com>
2024-12-14 16:05:56 -0800

    Update BooksList.js

--- src/components/Books/BooksList.js
+++ src/components/Books/BooksList.js
@@ -6,6 +6,23 @@ function BooksList() {
   const booksContext = useContext(BooksContext);
   const [ books, setBooks] = useState(booksContext);
 
+  const handleReserveButton = (bookId) => {
+  const selected = books.find((book) => book.bookId === bookId);
+  const index = books.indexOf(selected);
+  setBooks([
+    ...books.slice(0, index),
+    { ...selected, borrowed: true },
+    ...books.slice(index + 1),
+  ]);
+};
+
+  const bookList = books.map((book) => (
+  <Book
+    key={book.bookId}
+    book={book}
+    handleReserveButton={handleReserveButton}
+  />
+));
     return (
     <div className="container mt-5">
       <section className="d-flex gap-2 flex-wrap align-items-stretch justify-content-center">
